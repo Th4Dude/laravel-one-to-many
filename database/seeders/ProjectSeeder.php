46e007d3abee10adb1b06fcb8f98c77146077d37
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-22 17:59:46 +0300

    migrate + seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -17,7 +17,9 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        
+        Project::truncate();
+
         for($i = 0; $i < 10; $i++) {
             $new_project = new Project();
             $new_project->title = $faker->sentence(1);
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +17,16 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = ['Frontend', 'Backend', 'AI', 'DBA'];
+
+        
+        Type::truncate();
+        
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name);
+            $newType->save();
+        }
     }
 }
